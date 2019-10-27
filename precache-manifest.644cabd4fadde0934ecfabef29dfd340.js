381a7ae637c454e88220b01455ab55aa65f8e574
Catherine Claudet <cmclaudet@gmail.com>
2019-10-27 21:19:32 +0100

    Updates

---
+++ precache-manifest.644cabd4fadde0934ecfabef29dfd340.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9988be95d758fa19bde60f7417e708cf",
+    "url": "/wisp-child-website/./index.html"
+  },
+  {
+    "revision": "fba561050fa3e25caf69",
+    "url": "/wisp-child-website/./static/css/main.8f0e9739.chunk.css"
+  },
+  {
+    "revision": "b1eae4e0b847c0990b58",
+    "url": "/wisp-child-website/./static/js/2.409b5982.chunk.js"
+  },
+  {
+    "revision": "fba561050fa3e25caf69",
+    "url": "/wisp-child-website/./static/js/main.b99c6cb4.chunk.js"
+  },
+  {
+    "revision": "af85eac6f52edb2ee888",
+    "url": "/wisp-child-website/./static/js/runtime~main.802a5994.js"
+  },
+  {
+    "revision": "c4b9dd20de3f55a3630aacc24437d086",
+    "url": "/wisp-child-website/./static/media/ademia-and-mylo.c4b9dd20.png"
+  },
+  {
+    "revision": "59ab156e05d65d565f168d05f0fc33ec",
+    "url": "/wisp-child-website/./static/media/ademia-dialogue.59ab156e.png"
+  },
+  {
+    "revision": "f8419ae12e6d7cddce57d89cf5e2cd63",
+    "url": "/wisp-child-website/./static/media/ademia-neutral.f8419ae1.png"
+  },
+  {
+    "revision": "f7d8366e669fd6c2408f2d0b0b85f651",
+    "url": "/wisp-child-website/./static/media/arthur-neutral.f7d8366e.png"
+  },
+  {
+    "revision": "4cf7916ddbcfa5f41df6cd45a2fb6eb2",
+    "url": "/wisp-child-website/./static/media/buildings.4cf7916d.png"
+  },
+  {
+    "revision": "a2c060dfa6b170890a47fbe885e5e54e",
+    "url": "/wisp-child-website/./static/media/catherine-picture.a2c060df.jpg"
+  },
+  {
+    "revision": "78abd247f5fda5f97d1a42ac066ab95e",
+    "url": "/wisp-child-website/./static/media/email-logo.78abd247.png"
+  },
+  {
+    "revision": "b14c0866282ae94b57db36a268f5a533",
+    "url": "/wisp-child-website/./static/media/main-characters.b14c0866.png"
+  },
+  {
+    "revision": "641640f1b039877d1c91a30f4ffffd1c",
+    "url": "/wisp-child-website/./static/media/mylo-and-building.641640f1.png"
+  },
+  {
+    "revision": "1aacd2fb07c0b8002668ddcab1430217",
+    "url": "/wisp-child-website/./static/media/mylo-neutral.1aacd2fb.png"
+  },
+  {
+    "revision": "0fa0ddf36fe315a4eac166fae22df61e",
+    "url": "/wisp-child-website/./static/media/mylo-tree.0fa0ddf3.png"
+  },
+  {
+    "revision": "ec91f52695caeafb8ba1e42437a715bd",
+    "url": "/wisp-child-website/./static/media/nate-neutral.ec91f526.png"
+  },
+  {
+    "revision": "2b587d0067a8a70b307e577caef116d5",
+    "url": "/wisp-child-website/./static/media/nate-shout.2b587d00.png"
+  },
+  {
+    "revision": "f07220fef38fa8cd1c25c2370e7210b4",
+    "url": "/wisp-child-website/./static/media/orchard.f07220fe.png"
+  },
+  {
+    "revision": "fd84261b6bc99bb1d582f92b0ee305b4",
+    "url": "/wisp-child-website/./static/media/title.fd84261b.png"
+  },
+  {
+    "revision": "2451ce303795b690a648060a3d42d925",
+    "url": "/wisp-child-website/./static/media/twitter-logo.2451ce30.png"
+  },
+  {
+    "revision": "2912ab696c8860e4c7069d99ff776d22",
+    "url": "/wisp-child-website/./static/media/village-girl.2912ab69.png"
+  }
+]);
